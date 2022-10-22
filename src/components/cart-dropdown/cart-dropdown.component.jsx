53adfef1e8a35a205833403ebd71e1e769c94cb3
Diego <diegozmendez@gmail.com>
2022-10-22 17:45:04 -0400

    Add CartContext

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,14 @@
-import Button from '../button/button.component'
+import React from 'react';
 
-import './cart-dropdown.styles.scss'
+import Button from '../button/button.component';
 
-const CartDropdown = () => {
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    );
-};
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => (
+  <div className='cart-dropdown-container'>
+    <div className='cart-items' />
+    <Button>GO TO CHECKOUT</Button>
+  </div>
+);
 
 export default CartDropdown;
