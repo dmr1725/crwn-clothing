1dc200c8fb02346d781dc43de02ad150c477f3b3
Diego <diegozmendez@gmail.com>
2022-11-02 13:56:20 -0400

    Add createAction

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
+import { createAction } from "../utils/firebase/reducer/reducer.utils";
 
 export const UserContext = createContext({
     currentUser: null,
@@ -33,7 +34,9 @@ export const UserProvider = ({ children }) => {
     const { currentUser } = state
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+        dispatch(
+            createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
+        );
     }
 
     const value = { currentUser, setCurrentUser}
