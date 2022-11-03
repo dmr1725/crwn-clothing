517fde54850f5e98e7a4dbbac63d0dd0ad94e82f
Diego <diegozmendez@gmail.com>
2022-11-03 12:22:30 -0400

    Add Redux Dev Tools and move custom middleware

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,11 @@
+export const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type){
+        return next(action);
+    }
+    console.log(action.type);
+    console.log(action.payload);
+    console.log(store.getState());
+
+    next(action);
+    console.log('next state', store.getState());
+};
