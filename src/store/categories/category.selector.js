77435c5f8627fe88e7be56e5f16167732823264b
Diego <diegozmendez@gmail.com>
2022-11-02 21:53:08 -0400

    Add createSelector for categoriesReducer to memoize results/state

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,28 @@
-export const selectCategoriesMap = (state) => 
-    state.categories.categories.reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc
-    }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => {
+    // console.log('selector fired 1')
+    return  state.categories;
+}
+
+// createSelector is used to memoize/cache results
+// I think that it will not re-render or re-compute if categories hasn't change
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => {
+        // console.log('selector fired 2')
+        return categoriesSlice.categories
+    }
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        // console.log('selector fired 3');
+        return categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc
+        }, {})
+    }
+);
