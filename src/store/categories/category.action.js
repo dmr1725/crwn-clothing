7a86926140d3c65e973af8224ffae7b5671c2a57
Diego <diegozmendez@gmail.com>
2022-11-03 17:41:32 -0400

    Fix bug by copy pasting the course repo

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,24 +1,14 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from './category.types';
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-
-
-export const setCategories = (categoriesArray) => 
-    createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+import { createAction } from '../../utils/reducer/reducer.utils';
 
 export const fetchCategoriesStart = () =>
-    createAction(
-        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START
-    );
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 
 export const fetchCategoriesSuccess = (categoriesArray) =>
-    createAction(
-        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-         categoriesArray
-    );
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
 
-export const fetchCategoriesFailed = (error) =>
-    createAction(
-        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
-        error
-    );
+export const fetchCategoriesFailure = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
